38460e0f22f2db6e89048ed795ec6cfccbd31f1b
happyCoda <ypworkspace@gmail.com>
2013-11-29 23:35:20 +0400

    minor changes

--- js/app.js
+++ js/app.js
@@ -1,36 +1,5 @@
-var CarSelectorView = Purr.MVC.view(),
-CarSelectorModel = Purr.MVC.model(),
-CarSelectorController = Purr.MVC.controller({
-	model: CarSelectorModel,
-	view: CarSelectorView
-}),
-data = [{make: 'Ford', makeId: 1, models: ['Mustang', 'Focus']}, {make: 'MersedesBenz', makeId: 2, models: ['SLK']}, {make: 'Toyota', makeId: 3, models: ['Corolla']}];
-
-
-
-$(function () {
-	var tpl = $('.tpl').text();
-
-	console.log('parsed tpl', Purr.MVC.tpl.parse(tpl, data));
-});
-
-
-var a1 = ['Berlin', 'Zurich', 'Antverpen', 'Brugge', 'Monaco', 'Prague'];
-
-a1 = Purr.list(a1);
-
-//a1.removeFirst();
-a1.remove('Zurich', 'Monaco');
-console.log(Purr.util.isArray(a1.getRawArray()));
-
 var stringWithWildcards = 'Hello, mr. %s! You are %d now!',
 name = 'Smith',
 age = 30;
 Purr.string.format(stringWithWildcards, name, age);
-
-function delayed(name, greet) {
-	console.log(greet + ' ' + name + '!');
-}
-
-setTimeout(delayed, 1000, 'World', 'Hello');
 
