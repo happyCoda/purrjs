17e8015915213d4199d2b48703bd2a83018646f5
happyCoda <ypworkspace@gmail.com>
2016-02-10 01:26:44 +0300

    Unique, slice, extend and mixin methods added. Each method implemented.

--- js/utils.js
+++ js/utils.js
@@ -9,23 +9,64 @@ var Utils = {
     return toStr.call(something).replace(/\[|\]/g, '').split(' ')[1];
   },
 
-  each: function (iterable, callback) {
+  each: function (iterable, callback, context) {
     var iterableType = this.getType(iterable);
 
     if (iterableType === 'Array') {
 
-      iterable.forEach(callback);
+      iterable.forEach(callback, context);
 
     } else if (iterableType === 'Object') {
       Object.keys(iterable).forEach(function (key) {
-        callback(iterable[key], key);
+        callback(iterable[key], key, context);
       });
     } else {
       throw new Error('Argument must be an Object or an Array');
     }
   },
 
-  unique: function (raw) {}
+  unique: function (raw) {
+    var i = 0,
+      j,
+      len = raw.length,
+      unique = [];
+
+
+      for (; i < len; i += 1) {
+        for (j = 0; j <= i; j += 1) {
+          if (unique[j] === raw[i]) {
+            break;
+          } else if (j === i) {
+            unique.push(raw[i]);
+          }
+        }
+      }
+
+    return unique;
+  },
+
+  slice: function (arrayLike) {
+    
+    return Array.prototype.slice.call(arrayLike);
+  },
+
+  extend: function (extendable, extension) {
+    this.each(extension, function (val, prop) {
+      extendable[prop] = val;
+    });
+
+    return extendable;
+  },
+
+  mixin: function (extendable) {
+    var mixins = Array.prototype.slice.call(arguments);
+
+    this.each(mixins, function (mixin) {
+      this.extend(extendable, mixin);
+    }, this);
+
+    return extendable;
+  }
 };
 
 if (typeof define === 'function' && define.amd) {
--- test/specs/utilsSpec.js
+++ test/specs/utilsSpec.js
@@ -3,24 +3,24 @@
 describe('Testing Utils.js', function () {
   it('should be an object', function () {
 
-    expect(typeof Utils).toBe('object');
+    expect(typeof Utils).toEqual('object');
   });
 
-  it('should have a getType method', function () {
+  it('should implement a getType method', function () {
 
-    expect(typeof Utils.getType).toBe('function');
+    expect(typeof Utils.getType).toEqual('function');
 
   });
 
   it('should define correct type of the passed argument', function () {
 
-    expect(Utils.getType([])).toBe('Array');
+    expect(Utils.getType([])).toEqual('Array');
 
   });
 
-  it('should define each method', function () {
+  it('should implement an each method', function () {
 
-    expect(typeof Utils.each).toBe('function');
+    expect(typeof Utils.each).toEqual('function');
 
   });
 
@@ -43,9 +43,9 @@ describe('Testing Utils.js', function () {
 
   });
 
-  it('should have an unique method', function () {
+  it('should implement an unique method', function () {
 
-    expect(typeof Utils.unique).toBe('function');
+    expect(typeof Utils.unique).toEqual('function');
 
   });
 
@@ -53,5 +53,51 @@ describe('Testing Utils.js', function () {
 
     expect(Utils.unique([1, 2, 3, 2])).toEqual([1, 2, 3]);
 
+  });
+
+  it('should implement an multitype slice method', function () {
+
+    expect(typeof Utils.slice).toEqual('function');
+
+  });
+
+  it('should transform an argument into an array', function () {
+    var arrayLike = {'0': 'foo', '1': 'bar'},
+      transformed = Utils.slice(arrayLike);
+
+    expect(Utils.getType(transformed)).toEqual('Array');
+
+  });
+
+  it('should implement an extend method', function () {
+    var simpleObj = {
+        name: 'Conan'
+      },
+      largeObj = {
+        age: 23,
+        klass: 'barbarian'
+      };
+
+    expect(Utils.extend(simpleObj, largeObj).klass).toEqual('barbarian');
+  });
+
+  it('should implement a mixin method', function () {
+    var simpleObj = {
+        name: 'Conan'
+      },
+      largeObj = {
+        age: 23,
+        klass: 'barbarian'
+      },
+      superObj = {
+        skills: {
+          'attack': 32,
+          'defence': 24,
+          'wisdom': 7
+        },
+        weapon: 'two-handed sword'
+      };
+
+    expect(Utils.mixin(simpleObj, largeObj, superObj).weapon).toEqual('two-handed sword');
   });
 });
