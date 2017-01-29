1619fa85c61033c8257ca1a845bfa904281d5939
happyCoda <ypworkspace@gmail.com>
2017-01-29 12:57:11 +0300

    core library changes

--- public/js/src/modules/purr.js
+++ public/js/src/modules/purr.js
@@ -11,11 +11,17 @@ import EventBus from './bus';
 
 // let Purr = Purr || {};
 
-class Purr {
-  constructor() {
+function Purr () {}
+// Purr._pr = Purr.prototype;
 
-  }
-}
+// TODO: add opportunity to call constructors without new keyword
+Purr.prototype = new Stream ((_pr) => {
+  return _pr;
+}, Purr.prototype).pipe((_pr) => {
+  return Object.create(_pr, {});
+}).pipe((_pr) => {
+  return utils.mixin(_pr, Stream.prototype, EventBus.prototype);
+});
 
 
 /*
